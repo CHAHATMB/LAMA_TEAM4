317cf3efabe6828a9619563bc08cbe8b53a85004
Chahu <chahatmbaghele@gmail.com>
2023-08-10 23:09:47 +0530

    adding react folder

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Salary from './Salary';
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+      <br/><br/>
+       Hey Wells!!<br/><br/><br/><br/><br/><br/>
+      <Salary/>
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Salary.js
@@ -0,0 +1,8 @@
+
+export default function Salary(){
+    return(
+        <div>Your Next Salary will Arrive on
+            <h3>26 Aug 2023</h3>
+        </div>
+    );
+}
