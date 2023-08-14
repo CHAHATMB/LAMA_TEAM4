ee5a030521061a0b5415afa821a9dcf5fd782eba
Shruti Manohar Tanpure <62892396+shruti-tanpure@users.noreply.github.com>
2023-08-14 17:31:22 +0530

    Update App.js
    added routes

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import AdminLogin from './Components/AdminLogin';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import EmployeeLogin from './Components/EmployeeLogin';
 import AdminDashboard from './Components/AdminDashboard';
 function App() {
-  return (
-    <>
-    <Header/>
-    {/* <AdminDashboard/> */}
-    <AdminLogin/>
-    {/* <EmployeeLogin/> */}
-    <Footer/>
-    </>
-    
-  );
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginAsk/>}/>
+        <Route path="/adminLogin" element={<AdminLogin/>}/>
+        <Route path="/employeeLogin" element={<EmployeeLogin/>}/>
+        <Route path="/adminDashboard" element={<AdminDashboard/>}/>
+      </Routes>
+    </Router>
+   
+  )
 }
 
 export default App;
