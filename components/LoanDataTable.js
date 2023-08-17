4e3cd4021681eb8327c2d4d3869b8e944ca667b1
Shruti Manohar Tanpure <62892396+shruti-tanpure@users.noreply.github.com>
2023-08-17 17:56:06 +0530

    connected with backend

--- components/LoanDataTable.js
+++ components/LoanDataTable.js
@@ -4,23 +4,40 @@ import Button from 'react-bootstrap/Button';
 import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 //import './LoanDataTable.css'; // Import the CSS file
 import Header from './Header';
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 
 function LoanDataTable() {
-  const dummyLoanData = [
-    {
-      id: 1,
-      loanId: 'L001',
-      loanType: 'Personal Loan',
-      duration: 5,
-    },
-    {
-      id: 2,
-      loanId: 'L002',
-      loanType: 'Home Loan',
-      duration: 15,
-    },
-    // Add more dummy loan data entries here
-  ];
+    const[data, setData] = useState([]);
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://172.20.0.54:8080/api/loancards');
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+//   const dummyLoanData = [
+//     {
+//       id: 1,
+//       loanId: 'L001',
+//       loanType: 'Personal Loan',
+//       duration: 5,
+//     },
+//     {
+//       id: 2,
+//       loanId: 'L002',
+//       loanType: 'Home Loan',
+//       duration: 15,
+//     },
+//     // Add more dummy loan data entries here
+//   ];
 
   return (
     <div>
@@ -36,11 +53,11 @@ function LoanDataTable() {
             </tr>
           </thead>
           <tbody>
-            {dummyLoanData.map((item) => (
+            {data.map((item) => (
               <tr key={item.id}>
-                <td>{item.loanId}</td>
-                <td>{item.loanType}</td>
-                <td>{item.duration}</td>
+                <td>{item.loan_id}</td>
+                <td>{item.loan_type}</td>
+                <td>{item.duration_in_year}</td>
                 <td>
                   <Button variant="info" size="sm" className="me-2">
                     <AiOutlineEdit />
