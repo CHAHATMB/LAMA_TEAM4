c82ca4b992b5b46687d6338218aed86e39ee551f
sanika197 <74913116+sanika197@users.noreply.github.com>
2023-08-17 23:23:19 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,9 @@
-
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import NavLink from 'react-bootstrap/esm/NavLink';
+import logo from "../images/LamaL2.png";
 
 // background:'#FCD12A'
 function Header() {
@@ -10,18 +11,19 @@ function Header() {
     <Navbar style={{background:'#48b4bb'}}>
     <Container className='Container'>
 
-      <Navbar.Brand >
-      {/* <img src={LogoCH} width='8%' height='5%' marginTop='1%' marginLeft='2%' style={{paddingRight:'10px'}}/> */}
+      <Navbar.Brand style={{display:"flex", flexWrap:"wrap"}}>
+      <img src={logo} width='18%' height='5%' marginTop='1%' marginLeft='2%' style={{paddingRight:'10px'}}/>
       <h3 style={{color:'white', marginTop:'1%'}}>Loan Management</h3>
       </Navbar.Brand>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        {/* <Nav className="ms-auto">
-          <Nav.Link ><NavLink to='/About' exact style={{color:'white', float:'right', textDecoration:'none'}}>About</NavLink></Nav.Link>
-          <Nav.Link ><NavLink to='/ContactUs' exact style={{color:'white', textDecoration:'none'}}>Contact us</NavLink></Nav.Link>
+        <Nav className="ms-auto">
+        <Nav.Link ><NavLink to='/LoginAsk' exact style={{color:'white', float:'right', textDecoration:'none', fontSize:"19px"}}>Home</NavLink></Nav.Link>
+          <Nav.Link ><NavLink to='/About' exact style={{color:'white', float:'right', textDecoration:'none',fontSize:"19px"}}>About</NavLink></Nav.Link>
+          <Nav.Link ><NavLink to='/ContactUs' exact style={{color:'white', textDecoration:'none',fontSize:"19px"}}>Contact us</NavLink></Nav.Link>
 
-        </Nav> */}
+        </Nav>
       </Navbar.Collapse>
     </Container>
   </Navbar>
