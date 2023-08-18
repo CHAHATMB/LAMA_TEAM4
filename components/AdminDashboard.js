7acab8c55f36fc0580244a2c9631f2bb01fcf878
Shruti Manohar Tanpure <62892396+shruti-tanpure@users.noreply.github.com>
2023-08-18 13:27:17 +0530

    added button

--- components/AdminDashboard.js
+++ components/AdminDashboard.js
@@ -5,11 +5,18 @@ import Card from 'react-bootstrap/Card';
 import card1 from '../images/cardEmp.jpg';
 import card2 from '../images/card2Loan.jpg';
 import card3 from '../images/card3item.jpg';
+import { useNavigate } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 
 
 function AdminDashboard() {
+
+    let navigate = useNavigate();
   return (
+    <div>
+    <Header/>
     <div style={{display:'flex', flexWrap:'wrap'}}>
       <Card style={{ width: '27%', margin:"3%", marginTop:"4%",border: "1px solid rgba(255, 255, 255, .25)",
   borderRadius: "20px", boxShadow: "0 0 10px 1px rgba(0, 0, 0, 0.25)"}}>
@@ -21,7 +28,11 @@ function AdminDashboard() {
           bulk of the card's content.
         </Card.Text> */}
 
-        <Button variant="outline-info" style={{backgroundColor:'white', color:'#48b4bb'}}>View more</Button>
+        <Button variant="outline-info" style={{backgroundColor:'white', color:'#48b4bb'}}
+        onClick={() => navigate('/addUserData')}>Add Customer</Button>
+
+        <Button variant="outline-warning" style={{backgroundColor:'white', color:'#D19900', marginLeft:"8%"}}
+        onClick={() => navigate('/userDataTable')}>View Customer Table</Button>
       </Card.Body>
     </Card>
 
@@ -34,7 +45,10 @@ function AdminDashboard() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text> */}
-        <Button variant="outline-info" style={{backgroundColor:'white', color:'#48b4bb'}}>View more</Button>
+        <Button variant="outline-info" style={{backgroundColor:'white', color:'#48b4bb'}}
+        onClick={() => navigate('/loanCardAdd')}>Add loan Card</Button>
+        <Button variant="outline-warning" style={{backgroundColor:'white', color:'#D19900', marginLeft: "12%"}}
+        onClick={() => navigate('/loanDataTable')}>View Loan Table</Button>
       </Card.Body>
     </Card>
 
@@ -47,9 +61,14 @@ function AdminDashboard() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text> */}
-        <Button variant="outline-info" style={{backgroundColor:'white', color:'#48b4bb'}}>View more</Button>
+        <Button variant="outline-info" style={{backgroundColor:'white', color:'#48b4bb'}}
+        onClick={()=>navigate('/itemMasterAdd')}>Add item</Button>
+        <Button variant="outline-warning" style={{backgroundColor:'white', color:'#D19900', marginLeft:"10%"}}
+        onClick={() => navigate('/itemMasterDataTable')}>View Item Master Table</Button>
       </Card.Body>
     </Card>
+    </div>
+    <Footer/>
     </div>
   )
 }
