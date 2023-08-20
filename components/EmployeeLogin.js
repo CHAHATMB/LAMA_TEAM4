d6bd160ea7a954e5f89891347d315ce61e202952
sanika197 <74913116+sanika197@users.noreply.github.com>
2023-08-20 23:33:51 +0530

    Integrated with backend

--- components/EmployeeLogin.js
+++ components/EmployeeLogin.js
@@ -1,16 +1,20 @@
-
 import React from 'react'
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AdminLogin.css';
 import employee from '../images/employee.jpg';
 import NavLink from 'react-bootstrap/esm/NavLink';
+import Header from './Header';
+import Footer from './Footer';
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function EmployeeLogin() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const storeEmail = (e) =>{
     setEmail(e.target.value);
@@ -19,9 +23,34 @@ function EmployeeLogin() {
   const storePassword = (e) =>{
     setPassword(e.target.value);
   }
+
+  function handleClick(e){
+    axios({
+     method: 'POST',
+     url: 'http://172.20.0.54:8080/api/auth/login',
+     data:{
+       email: email,
+       password: password
+     } ,
+    }).then((data)=>{
+      console.log(data);
+      axios.defaults.headers.common.Authorization = "Bearer " + data.data.id_token;
+      if(data.data.role === "USER"){
+      console.log("Allow user");
+      navigate('/employeeDashboard');
+      }
+      else{
+        navigate('/')
+      }
+     
+   })
+    console.log("Submit")
+ }
   return (
+    <>
+    <Header/>
     <div style={{display:"flex",flexWrap:"wrap"}}>
-    <img src={employee} style={{height:"70%", width:"58.5%"}}/>
+    <img src={employee} style={{height:"75%", width:"60%"}}/>
     <div style={{ width:'22rem', padding:'2px', marginTop:'5%', marginLeft:'5%' ,overflowY:'auto', height:'24rem'}}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Login to your Account</h3>
@@ -46,9 +75,9 @@ function EmployeeLogin() {
           </div>
         
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn"  style={{marginTop:"7%", width:"18rem"}}>
+            <Button variant="primary" onClick={handleClick} style={{marginTop:"7%", width:"18rem", backgroundColor:"#48b4bb"}}>
                 Submit
-            </button>
+            </Button>
           </div>
                
 
@@ -58,6 +87,8 @@ function EmployeeLogin() {
         </div>
       </div>
       </div>
+      <Footer/>
+      </>
   )
 }
 
