fb05f68a541dcc8e06db9351766b1e390eff5e47
sanika197 <74913116+sanika197@users.noreply.github.com>
2023-08-22 16:22:35 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,25 +4,35 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import NavLink from 'react-bootstrap/esm/NavLink';
 import logo from "../images/LamaL2.png";
+import { useNavigate } from 'react-router-dom';
 
 // background:'#FCD12A'
 function Header() {
+
+  const navigate = useNavigate();
+  function handleHome(){
+    navigate("/");
+  }
+
+
+  function handleAbout(){
+
+  }
   return (
     <Navbar style={{background:'#48b4bb'}}>
     <Container className='Container'>
 
       <Navbar.Brand style={{display:"flex", flexWrap:"wrap"}}>
       <img src={logo} width='18%' height='5%' marginTop='1%' marginLeft='2%' style={{paddingRight:'10px', color:"white"}}/>
-      <h3 style={{color:'white', marginTop:'1%'}}>Loan Management</h3>
+      <h3 onClick = {handleHome} style={{cursor:"pointer", color:'white', marginTop:'1%'}}>Loan Management</h3>
       </Navbar.Brand>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-        <Nav.Link ><NavLink to='/LoginAsk' exact style={{color:'white', float:'right', textDecoration:'none', fontSize:"19px"}}>Home</NavLink></Nav.Link>
-          <Nav.Link ><NavLink to='/About' exact style={{color:'white', float:'right', textDecoration:'none',fontSize:"19px"}}>About</NavLink></Nav.Link>
-          <Nav.Link ><NavLink to='/ContactUs' exact style={{color:'white', textDecoration:'none',fontSize:"19px"}}>Contact us</NavLink></Nav.Link>
-
+        <h5 onClick={handleHome} style={{color:'white', float:'right', textDecoration:'none', fontSize:"19px", cursor:"pointer"}}>Home</h5>
+        <h5 onClick={handleAbout} style={{color:'white', float:'right', textDecoration:'none',fontSize:"19px", marginLeft:"20%", cursor:"pointer"}}>About</h5>
+          
         </Nav>
       </Navbar.Collapse>
     </Container>
